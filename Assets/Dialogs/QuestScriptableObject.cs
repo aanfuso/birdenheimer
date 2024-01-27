21d976a3c4d935e16b4f8f3a8ec0a1db4a192cb2
Agustin Anfuso <anfusoagustin89@gmail.com>
2024-01-27 18:30:02 +0400

    Add Initial Quest SO and Dialogs

---
+++ Assets/Dialogs/QuestScriptableObject.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "QuestScriptableObject", menuName = "ScriptableObjects/QuestScriptableObject")]
+public class QuestScriptableObject : ScriptableObject
+{
+    public string textBody;
+    public Sprite sprite;
+}
