29a1da1b36f939c3841d6fda149ba51a4ac27673
Agustin Anfuso <anfusoagustin89@gmail.com>
2024-01-28 11:36:13 +0400

    Add UI event constants

---
+++ Assets/UI/UIEvents.cs
@@ -0,0 +1,11 @@
+public static class UIEvents
+{
+    public const string PAUSE = "pause";
+
+    public const string HELP_SHOW = "help:show";
+    public const string QUEST_SHOW = "quest:show";
+    public const string SETTINGS_SHOW = "settings:show";
+
+    public const string ACTION_ATE = "action:ate";
+    public const string ACTION_BOUNCED = "action:bounced";
+}
